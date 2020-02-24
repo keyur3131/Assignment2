8f77c91dfd2a1ed9f6dbd22c6850af58f149b761
Keyur Navadiya <keyurnavadiya3131@gmail.com>
2020-02-24 15:53:43 -0500

    First commit

---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select a number from below");
+            Console.WriteLine("1.Enter Triangle dimention");
+            Console.WriteLine("2. Exit");
+
+            string userInput = Console.ReadLine();
+            string inputOne, inputTwo, inputThree;
+
+            switch (userInput)
+            {
+                case "1":
+                    Console.WriteLine("Please enter 1st side of triangle.");
+                    inputOne = Console.ReadLine();
+                    Console.WriteLine("Please enter 2nd side of triangle.");
+                    inputTwo = Console.ReadLine();
+                    Console.WriteLine("Please enter 3rd side of triangle.");
+                    inputThree = Console.ReadLine();
+                    //if (TriangleSolver.Analyze(sideOne,sideTwo,sideThree))
+                    //{
+
+                    //}
+                    break;
+                case "2":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Please select a number from below");
+                    Console.WriteLine("1.Enter Triangle dimention");
+                    Console.WriteLine("2. Exit");
+                    break;
+
+
+            }
+        }
+    }
+}
---
+++ Assignment2/Assignment2/TriangleSolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public static class TriangleSolver
+    {
+        public static int Analyze(int sideOne, int sideTwo, int sideThree)
+        {
+            if (sideOne + sideTwo <= sideThree || sideOne + sideThree <= sideTwo ||
+                            sideTwo + sideThree <= sideOne)
+            {
+                return 0;
+            }
+            else
+                return 1;
+        }
+    }
+}
